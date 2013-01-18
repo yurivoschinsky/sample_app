2c5757c28638e30da0a5d83bf2d9fcfdf02d9ea7
Yura V <y@slimdb.com>
2013-01-18 10:04:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd2edaf45a086521b32f35e4c845f7114f8105e7a3d832bbeb06fdd5a90a216eff385b2dc54e3aa90426de6333c9178a50db04b463ec0b29291254eda3c252e5c'
